608a20dc0532224afc980a31faaa1a62f188a3c4
secret_online <me@secretonline.co>
2024-12-30 09:52:27 +1300

    Remove unused main entrypoint in favour of keeping everything in client

--- src/client/java/co/secretonline/accessiblestep/AccessibleStepClient.java
+++ src/client/java/co/secretonline/accessiblestep/AccessibleStepClient.java
@@ -1,9 +1,21 @@
 package co.secretonline.accessiblestep;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.util.Identifier;
 
 public class AccessibleStepClient implements ClientModInitializer {
+	public static final String MOD_ID = "accessible-step";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static Identifier id(String path) {
+		return Identifier.of(MOD_ID, path);
+	}
+
 	@Override
 	public void onInitializeClient() {
 		AccessibleStepEndTick endTickListener = new AccessibleStepEndTick();
