9a9ab87e15176b1d690588677b7b4c2b5d46a50c
secret_online <me@secretonline.co>
2026-09-03 21:01:40 +1200

    Backport to 26.1

--- fabric/src/gametest/java/co/secretonline/accessiblestep/fabric/gametest/client/StepModeAutoJumpTest.java
+++ fabric/src/gametest/java/co/secretonline/accessiblestep/fabric/gametest/client/StepModeAutoJumpTest.java
@@ -18,7 +18,7 @@ public class StepModeAutoJumpTest implements FabricClientGameTest {
 			ClientTestHelper.placeTestStructure(worldContext, startPosition);
 			ClientTestHelper.setStepMode(testContext, StepMode.AUTO_JUMP);
 
-			worldContext.getConnection().waitForChunksRender();
+			worldContext.getClientLevel().waitForChunksRender();
 
 			testContext.getInput().holdKeyFor(GLFW.GLFW_KEY_W, 20);
 
@@ -27,8 +27,8 @@ public class StepModeAutoJumpTest implements FabricClientGameTest {
 			testContext.runOnClient((client) -> {
 				BlockPos endPosition = client.player.blockPosition();
 				Block block = client.player.level().getBlockState(endPosition.below()).getBlock();
-				if (!block.equals(Blocks.DYED_TERRACOTTA.yellow())) {
-					throw new AssertionError(String.format("Incorrect block. Expected: %s, got %s", Blocks.DYED_TERRACOTTA.yellow(), block));
+				if (!block.equals(Blocks.YELLOW_TERRACOTTA)) {
+					throw new AssertionError(String.format("Incorrect block. Expected: %s, got %s", Blocks.YELLOW_TERRACOTTA, block));
 				}
 			});
 		}
