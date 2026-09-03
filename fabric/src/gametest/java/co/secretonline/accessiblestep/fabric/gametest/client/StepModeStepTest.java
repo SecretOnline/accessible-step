f3f9b79d4f3ae81830eb3bdbcb5b01ef124d1fce
secret_online <me@secretonline.co>
2026-09-03 18:49:47 +1200

    Update client gametest API calls

--- fabric/src/gametest/java/co/secretonline/accessiblestep/fabric/gametest/client/StepModeStepTest.java
+++ fabric/src/gametest/java/co/secretonline/accessiblestep/fabric/gametest/client/StepModeStepTest.java
@@ -18,7 +18,7 @@ public class StepModeStepTest implements FabricClientGameTest {
 			ClientTestHelper.placeTestStructure(worldContext, startPosition);
 			ClientTestHelper.setStepMode(testContext, StepMode.STEP);
 
-			worldContext.getClientLevel().waitForChunksRender();
+			worldContext.getConnection().waitForChunksRender();
 
 			testContext.getInput().holdKeyFor(GLFW.GLFW_KEY_W, 20);
 
